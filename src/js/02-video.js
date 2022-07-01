84535f83cc0bb8f3ea05749e8866712ac886764e
Andriy Makarov <angeltek69@gmail.com>
2022-07-01 11:38:20 +0200

    Final

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const savedData = localStorage.getItem('videoplayer-current-time');
+const parsedData = JSON.parse(savedData);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(parsedData.seconds).catch(function (error) {
+  switch (error.name) {
+    case 'RangeError':
+      break;
+
+    default:
+      break;
+  }
+});
+
+function onPlay() {
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      localStorage.setItem(
+        'videoplayer-current-time',
+        JSON.stringify({ seconds })
+      );
+      console.log(seconds);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+populateForm();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const formData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  console.log(formData);
+  evt.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function onFormInput(e) {
+  let savedForm = localStorage.getItem('feedback-form-state');
+
+  if (savedForm) {
+    savedForm = JSON.parse(savedForm);
+  } else {
+    savedForm = {};
+  }
+
+  savedForm[e.target.name] = e.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(savedForm));
+}
+
+function populateForm() {
+  let savedForm = localStorage.getItem('feedback-form-state');
+  if (savedForm) {
+    savedForm = JSON.parse(savedForm);
+    form.elements.email.value = savedForm.email ? savedForm.email : '';
+    form.elements.message.value = savedForm.message ? savedForm.message : '';
+  }
+}
